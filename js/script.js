bfeb57508c3634a0311d652ed03c88f0f22255bc
LuTyrn <lukas.tyrner@gmail.com>
2024-02-12 01:56:45 +0100

    Úprava menu pro menší displeje
    Přidání ikonky X pro zavření menu a úprava js, pro automatické skrytí menu po kliknutí na položku.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
+/*=== toggle icon navbar ===*/
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
 /*=== sroll sections active link ===*/
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
@@ -20,4 +29,8 @@ window.onscroll = () => {
     let header = document.querySelector('header');
 
     header.classList.toggle('sticky', window.scrollY > 100);
+
+    /*=== remove toggle icon and navbar when click navbar link (scroll) ===*/
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
 };
